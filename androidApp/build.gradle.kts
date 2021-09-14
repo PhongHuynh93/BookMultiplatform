631728fc94b59305dc8279ce910ab03983e70abd
Do Nguyen <p7157568@gmail.com>
2021-09-14 20:19:07 +0700

    Update lib version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,21 +1,15 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-}
-
-dependencies {
-    implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    kotlin("kapt")
 }
 
 android {
-    compileSdk = 31
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
     defaultConfig {
         applicationId = "com.wind.book.android"
-        minSdk = 23
-        targetSdk = 31
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
         versionCode = 1
         versionName = "1.0"
     }
@@ -24,4 +18,46 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf(
+            *freeCompilerArgs.toTypedArray(),
+            "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-Xopt-in=kotlinx.coroutines.FlowPreview",
+            "-Xopt-in=kotlin.Experimental",
+            "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi"
+        )
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+
+    //UI
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+
+    //Coroutines
+    val coroutinesVersion = properties["version.kotlinx.coroutines"]
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    //DI
+    implementation("org.koin:koin-core:2.2.2")
+    implementation("org.koin:koin-androidx-scope:2.2.2")
+
+    //Image load
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    kotlin("plugin.serialization")
 }
 
+// Use version for cocoaPod
 version = "1.0"
 
 kotlin {
@@ -28,30 +30,55 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                //Network
+                implementation("io.ktor:ktor-client-core:${findProperty("version.ktor")}")
+                implementation("io.ktor:ktor-client-logging:${findProperty("version.ktor")}")
+                //Coroutines
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${findProperty("version.kotlinx.coroutines")}")
+                //Logger
+                implementation("io.github.aakira:napier:2.1.0")
+                //JSON
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${findProperty("version.kotlinx.serialization")}")
+                //Key-Value storage
+                implementation("com.russhwolf:multiplatform-settings:0.8")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                //Network
+                implementation("io.ktor:ktor-client-okhttp:${findProperty("version.ktor")}")
+            }
+        }
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
             }
         }
-        val iosMain by getting
+        val iosMain by getting {
+            dependencies {
+                //Network
+                implementation("io.ktor:ktor-client-ios:${findProperty("version.ktor")}")
+            }
+        }
         val iosTest by getting
     }
 }
 
 android {
-    compileSdk = 31
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+
     defaultConfig {
-        minSdk = 23
-        targetSdk = 31
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
     }
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 }
