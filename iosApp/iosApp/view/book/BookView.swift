8e3caa0e4302d2964bb15e7a4dc56315c0064ea6
Phong Huynh <p7157568@gmail.com>
2021-09-19 13:21:39 +0700

    Add bottom navigation

---
+++ iosApp/iosApp/view/book/BookView.swift
@@ -0,0 +1,21 @@
+//
+//  BookView.swift
+//  iosApp
+//
+//  Created by Huynh Phong on 19/09/2021.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct BookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookView()
+    }
+}
