3a5dbcd9f62f8ede60d8fe8b745f325e6220fb2d
Phong Huynh <p7157568@gmail.com>
2021-09-22 01:02:50 +0700

    [Ios] Book - update design

---
+++ iosApp/iosApp/util/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  iosApp
+//
+//  Created by Huynh Phong on 21/09/2021.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// width/ height
+let bookRatio = CGFloat(2/3.0)
+let smallRadius = CGFloat(4.0)
