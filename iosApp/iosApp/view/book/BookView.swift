a712f8dd204b72c92bf0cccea842ef4e7b3c0e5d
Phong Huynh <p7157568@gmail.com>
2021-09-25 11:40:44 +0700

    iOS - add loading state

--- iosApp/iosApp/view/book/BookView.swift
+++ iosApp/iosApp/view/book/BookView.swift
@@ -17,39 +17,44 @@ struct BookView: View {
 
     var body: some View {
         VStack {
-            NavigationLink(destination: LazyView(IABView(iabNav: self.$iabNav)), isActive: self.$navIAB) {}
-            List(observable.state.data as! [Book], id: \.id) { book in
-                HStack(alignment: .top) {
-                    KFImage(URL(string: book.thumb.url))
-                        .placeholder {
-                            VStack {
-                                Color.gray
+            switch observable.state.loadState {
+            case is LoadState.Loading:
+                LoadingScreen()
+            default:
+                NavigationLink(destination: LazyView(IABView(iabNav: self.$iabNav)), isActive: self.$navIAB) {}
+                List(observable.state.data as! [Book], id: \.id) { book in
+                    HStack(alignment: .top) {
+                        KFImage(URL(string: book.thumb.url))
+                            .placeholder {
+                                VStack {
+                                    Color.gray
+                                }
                             }
-                        }
-                        .resizable()
-                        .aspectRatio(bookRatio, contentMode: .fill)
-                        .frame(width: 128)
-                        .cornerRadius(smallRadius)
-                        .clipped()
+                            .resizable()
+                            .aspectRatio(bookRatio, contentMode: .fill)
+                            .frame(width: 128)
+                            .cornerRadius(smallRadius)
+                            .clipped()
 
-                    VStack(alignment: .leading) {
-                        Text(book.title)
-                            .font(.headline)
-                            .lineLimit(1)
-                        Text(book.author)
-                            .font(.caption)
-                        Text(book.description_)
-                            .padding(.top, 2)
-                            .font(.body)
-                            .lineLimit(3)
-                        Spacer()
-                        Button("Buy") {
-                            KoinKt.log.d(withMessage: { "on tap buy" })
-                            observable.event.onClickBuy(book: book)
-                        }.buttonStyle(.bordered)
-                    }
-                }.padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
-            }.listStyle(.plain)
+                        VStack(alignment: .leading) {
+                            Text(book.title)
+                                .font(.headline)
+                                .lineLimit(1)
+                            Text(book.author)
+                                .font(.caption)
+                            Text(book.description_)
+                                .padding(.top, 2)
+                                .font(.body)
+                                .lineLimit(3)
+                            Spacer()
+                            Button("Buy") {
+                                KoinKt.log.d(withMessage: { "on tap buy" })
+                                observable.event.onClickBuy(book: book)
+                            }.buttonStyle(.bordered)
+                        }
+                    }.padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+                }.listStyle(.plain)
+            }
         }
         .navigationTitle("Book")
         .onAppear { observable.startObserving() }
