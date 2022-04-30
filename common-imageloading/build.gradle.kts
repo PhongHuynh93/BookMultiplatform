1512d437a50d8650709e3e4e8fdefab6faa4776e
Android Aliens <p7157568@gmail.com>
2022-04-30 20:10:05 +0700

    feat: [Android] Create common-imageloading module

---
+++ common-imageloading/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id(Plugins.androidLibrary)
+    kotlin(Plugins.kotlinAndroid)
+    kotlin(Plugins.kapt)
+}
+
+android {
+    compileSdk = Configs.compileSdk
+
+    defaultConfig {
+        minSdk = Configs.minSdk
+        targetSdk = Configs.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.androidX)
+    api(libs.coil.coil)
+}
